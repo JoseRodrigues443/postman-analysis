6db359079b5dfd8ee5ab117d0828bfe7e9806c95
José Rodrigues <josemiguel443@gmail.com>
2022-09-01 08:09:04 +0000

    Added document

--- src/api/document.handler.ts
+++ src/api/document.handler.ts
@@ -3,13 +3,13 @@ import { Document } from '../domain/document';
 import { User } from '../domain/user';
 
 function getDocumentById(request: any, response: any): Document {
-    console.log(request)
-    const id = request.params.id;
-    const user = new User('123')
-    const accesses = [
-        new Access(user)
-    ];
-    return response.status(200).json(new Document(id, accesses));
+	console.log(request)
+	const id = request.params.id;
+	const user = new User('123')
+	const accesses = [
+		new Access(user)
+	];
+	return response.status(200).json(new Document(id, accesses));
 }
 
 
